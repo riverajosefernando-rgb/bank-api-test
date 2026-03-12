6693080d7053927e58c1e23cad1f0a0dbaef2d96
riverajosefernando-rgb <rivera.jose.fernando@gmail.com>
2026-03-12 15:00:28 -0500

    Update KarateTest & Jenkinsfile

--- src/test/java/runners/KarateTest.java
+++ src/test/java/runners/KarateTest.java
@@ -1,30 +1,12 @@
 package runners;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import org.junit.jupiter.api.Test;
+import com.intuit.karate.junit5.Karate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+class KarateTest {
 
-public class KarateTest {
-
-    @Test
-    void runTests() {
-
-        String tags = System.getProperty("karate.options");
-
-        Runner.Builder runner = Runner.path("classpath:features");
-
-        if (tags != null) {
-            tags = tags.replace("@", "").trim();   // 👈 quitar @
-            runner = runner.tags(tags);
-        }
-
-        Results results = runner.parallel(5);
-
-        System.out.println("Features executed: " + results.getFeaturesTotal());
-        System.out.println("Scenarios executed: " + results.getScenariosTotal());
-
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    @Karate.Test
+    Karate runTests() {
+        return Karate.run("classpath:features");
     }
+
 }
