ca0d74bccd7cb6d843542314dd4decf4f7daf2dd
riverajosefernando-rgb <rivera.jose.fernando@gmail.com>
2026-03-12 11:45:55 -0500

    Fix KarateTest runner

--- src/test/java/runners/KarateTest.java
+++ src/test/java/runners/KarateTest.java
@@ -1,29 +1,12 @@
 package runners;
 
-import com.intuit.karate.Results;
-import com.intuit.karate.Runner;
-import org.junit.jupiter.api.Test;
+import com.intuit.karate.junit5.Karate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+class KarateTest {
 
-public class KarateTest {
-
-    @Test
-    void testAll() {
-
-        String karateOptions = System.getProperty("karate.options");
-
-        Runner.Builder runner = Runner.path("classpath:features");
-
-        if (karateOptions != null && karateOptions.contains("--tags")) {
-            String tag = karateOptions.replace("--tags", "").trim();
-            runner = runner.tags(tag);
-        }
-
-        Results results = runner.parallel(5);
-
-        System.out.println("Scenarios executed: " + results.getScenariosTotal());
-
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:features");
     }
+
 }
